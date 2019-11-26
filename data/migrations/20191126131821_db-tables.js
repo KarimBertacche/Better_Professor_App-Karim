4f523ccd74b6c1a8d6fe0e78ddf3d80d3f78cea4
Karim B <mattiakarim@live.it>
2019-11-26 13:31:53 +0100

    <✨> Created first table for user details

--- data/migrations/20191126131821_db-tables.js
+++ data/migrations/20191126131821_db-tables.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable('Users', table => {
+            table.increments();
+            table.string('first_name', 128).notNullable();
+            table.string('last_name', 128).notNullable();
+            table.string('email', 128).notNullable().unique();
+            table.string('password', 128).notNullable();
+            table.string('role', 128).notNullable();
+        })
+        .createTable('')
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('Users');
 };
