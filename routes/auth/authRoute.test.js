505bc71c53de10c011030c5dde0eeda636de78b8
Karim B <mattiakarim@live.it>
2019-11-26 11:56:49 +0100

    <🧪> Added test for register endpoint

---
+++ routes/auth/authRoute.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const server = require('../../API/server');
+
+describe('Auth Routes', () => {
+    describe('Testing register endpoint' () => {
+        it('validate body of the request', () => {
+            return request(server).post('/api/auth/register')
+                .expect(400)
+                .expect({ 
+                    message: 'Missing credentials to proceed with registration'
+                });
+        });
+
+        it('validate email of the request', () => {
+            return request(server).post('api/auth/register')
+                .send({ email: 'karim', password: 'badPassword'})
+                .expect(400)
+                .expect({
+                    message: 'Invalid email format'
+                });
+        });
+
+        it('validate if user first & last name are present', () => {
+            return request(server).post('api/auth/register')
+                .send({ email: 'karim@live.com', password: 'badPassword'})
+                .expect(400)
+                .expect({
+                    message: 'Missing credentials to proceed with registration'
+                });
+        });
+
+        it('validate if endpoint works correctly with right credentials', () => {
+            return request(server).post('api/auth/register')
+                .send({
+                    firstName: 'Karim',
+                    lastName: 'Bertacche',
+                    email: 'karim@live.com',
+                    password: 'badPassword',
+                    role: 'reviewer'
+                })
+                .expect(201);
+        });
+    });
+
+    describe('Testing login endpoint', () => {
+        it()
+    });
+});
