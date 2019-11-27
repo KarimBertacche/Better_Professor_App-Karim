3e96f67b3e644791988187c7c900c936c2c61039
Karim B <mattiakarim@live.it>
2019-11-27 18:17:16 +0100

    <✨> Connected all routes

--- API/server.js
+++ API/server.js
@@ -6,14 +6,24 @@ const cors = require('cors');
 
 const server = express();
 
-const PostRouter = require('./posts/post-router.js');
-
+const logger = require('../middlewares/logger');
+const restricted = require('../middlewares/restricted');
+const authRouter = require('../routes/auth/authRoute');
+const usersRouter = require('../routes/users/usersRouter');
+const studentsRouter = require('../routes/students/studentsRouter');
+const projectsRouter = require('./routes/projects/projectsRouter');
+const messagesRouter = require('./routes/messages/messagesRouter');
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use('/api/posts', PostRouter);
+server.use('/', logger);
+server.use('/auth', authRouter);
+server.use('/users', restricted, usersRouter);
+server.use('/students', restricted, studentsRouter);
+server.use('/projects', restricted, projectsRouter);
+server.use('/messages', restricted, messagesRouter);
 server.get('*', handleRequest);
 
 function handleRequest(req, res) {
