3f4f768aebbd87a9846511adbcac85fd25796fda
Karim B <mattiakarim@live.it>
2019-11-28 14:32:58 +0100

    Latest changes...

--- config/db-config.js
+++ config/db-config.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
-const config = require('../knexfile.js');
+const knexConfig = require('../knexfile');
 
-module.exports = knex(config.development);
+const environment = process.env.NODE_ENV || 'development'
+
+module.exports = knex(knexConfig[environment]);
