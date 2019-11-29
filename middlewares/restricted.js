45988d55794ac689f15a016c5f377e178d6a2472
Karim B <mattiakarim@live.it>
2019-11-29 11:18:28 +0100

    <🧪> Tested end-points...

--- middlewares/restricted.js
+++ middlewares/restricted.js
@@ -7,26 +7,25 @@ const {
 } = require('../helpers/variables');
 
 module.exports = (req, res, next) => {
-    next();
-    // const token = req.headers.authorization;
+    const token = req.headers.authorization;
 
-    // if(token) {
-    //     jwt.verify(
-    //         token,
-    //         secret.jwtSecret,
-    //         (err, decodedToken) => {
-    //             if(err) {
-    //                 res.status(401).json({ 
-    //                     message: invalidToken,
-    //                     error: err.message 
-    //                 });
-    //             } else {
-    //                 req.decodedToken = decodedToken;
-    //                 next();
-    //             }
-    //         }
-    //     )
-    // } else {
-    //     res.status(400).json({ message: missingCredentials });
-    // }
+    if(token) {
+        jwt.verify(
+            token,
+            secret.jwtSecret,
+            (err, decodedToken) => {
+                if(err) {
+                    res.status(401).json({ 
+                        message: invalidToken,
+                        error: err.message 
+                    });
+                } else {
+                    req.decodedToken = decodedToken;
+                    next();
+                }
+            }
+        )
+    } else {
+        res.status(400).json({ message: missingCredentials });
+    }
 };
